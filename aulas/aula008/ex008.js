6360396e4fd1755198a025e7fbafe034e361f967
Luiz Phelipe <luizphsantana@gmail.com>
2024-05-26 18:36:47 -0300

    .

---
+++ aulas/aula008/ex008.js
@@ -0,0 +1,9 @@
+var vel = 60;
+console.log(`A velocidade do seu carro é de ${vel}KM/h`);
+
+// Verifica se o carro está acima de 60KM/h
+if (vel > 60) { 
+    console.log(`Você ultrapassou o limite de velocidade! MULTADO`)
+} // Condição simples
+
+console.log(`Dirija sempre com sinto de segurança`);
---
+++ aulas/aula008/ex009.js
@@ -0,0 +1,8 @@
+var idade = 18;
+console.log(`Sua idade é ${idade}`);
+
+if (idade >= 18) { // Verifica se é maior de idade
+    console.log(`Você é maior de idade`); // Se a condição for True
+} else {
+    console.log(`Você é menor de idade`); // Se a condição for False
+}
